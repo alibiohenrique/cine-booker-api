ffa9e19e49152fcef8929c2eefd61d8349e2b235
alibio <alibio.henrique.com>
2024-10-07 17:51:41 -0300

    add: initial config

---
+++ src/main/java/com/cinebooker/api/CineBookerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinebooker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBookerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineBookerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinebooker/api/CineBookerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinebooker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBookerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
